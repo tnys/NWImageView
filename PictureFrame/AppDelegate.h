12f5268f276ab226e7d5a9e78dfedc0c5fccc869
Tom Nys <tom.nys@netwalk.be>
2012-12-13 21:18:13 +0100

    Initial Commit

---
+++ PictureFrame/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PictureFrame
+//
+//  Created by Tom Nys on 13/12/12.
+//  Copyright (c) 2012 Tom Nys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PictureFrame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PictureFrame
+//
+//  Created by Tom Nys on 13/12/12.
+//  Copyright (c) 2012 Tom Nys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
